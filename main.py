5cb9fe0e1e9228abfcd1f95912e32c93b22f4bcd
Adrian Wasilewski <wasilewskiadrian28@gmail.com>
2022-05-06 14:04:19 +0200

    Pierwsza wersja

---
+++ main.py
@@ -0,0 +1,28 @@
+import math as mt
+
+def pole_figury(*args):
+	if len(*args) < 4:
+		for num in args:
+			if len(*args) == 1:
+				return mt.pi * num[0] ** 2
+			elif len(*args) == 2:
+				return num[0] * num[1]
+			elif len(*args) == 3:
+				return (num[0] * num[1]) / 2
+		
+
+liczba_figur = int(input())
+lista_wartosci = []
+wynik = 0
+
+for l in range(liczba_figur):
+	lista_wartosci = [float(x) for x in input().split()]
+	try:
+		wynik = wynik + pole_figury(lista_wartosci)
+	except:
+		wynik = False
+		print("Błąd: można podać maksymalnie 3 liczby")
+		break;
+
+if wynik:
+	print(round(wynik, 2))
